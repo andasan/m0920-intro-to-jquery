c5d4e39bc8925761c5bc75c584d7793b65594b7a
Francois Polo <andasan@gmail.com>
2021-02-18 10:13:58 -0800

    completed jquery

--- 11-codebin/app.js
+++ 11-codebin/app.js
@@ -1,28 +1,4 @@
-$('.toggleButton').on('mouseenter', function(){
-    // $(this).css('background-color', 'grey')
-    $(this).addClass('highlightedButton')
-}).on('mouseleave', function(){
-    // $(this).css('background-color', '#EEE')
-    $(this).removeClass('highlightedButton')
-}).on('click', function(){
-    $(this).toggleClass('active')
-    $(this).removeClass('highlightedButton')
-
-    let panelId = $(this).attr('id') + 'Panel'
-    $('#' + panelId).toggleClass('hidden')
-
-    let numberofActivePanels = 4 - $('.hidden').length
-    $('.panel').width($(window).width() / numberofActivePanels)
-})
-
-$('.panel').height($(window).height() - $('#header').height() - 15)
-$('.panel').width($(window).width() / 2)
-
-$('iframe').contents().find('html').html('BYE')
-
-$('div').val('BYE')
-
-$('textarea').on('change keyup paste', function(){
+function updateOutput(){
     $('iframe').contents().find('html').html(`
         <html>
         <head>
@@ -35,4 +11,49 @@ $('textarea').on('change keyup paste', function(){
         </body>
         </html>
     `)
+
+    document.getElementById('outputPanel').contentWindow.eval($('#javascriptPanel').val())
+}
+
+//the following methods is invoked by different events by the buttons
+$('.toggleButton').on('mouseenter', function(){
+
+    $(this).addClass('highlightedButton')
+
+}).on('mouseleave', function(){
+
+    $(this).removeClass('highlightedButton')
+
+}).on('click', function(){
+
+    $(this).toggleClass('active')
+    $(this).removeClass('highlightedButton')
+
+    //get the id of the button that was clicked
+    let panelId = $(this).attr('id') + 'Panel'
+    //select the class of the selected panel
+    $('#' + panelId).toggleClass('hidden')
+
+    //count the number of elements with a certain class
+    //count the number that are hidden
+    let numberOfActivePanels = 4 - $('.hidden').length
+    $('.panel').width($(window).width() / numberOfActivePanels - 10)
+})
+
+//=====================================================================
+// The code below will be evaluated first when page loads/renders
+//=====================================================================
+
+//subtracts the window's height with the header's height and apply to the panel's height
+$('.panel').height($(window).height() - $('#header').height() - 15)
+
+//evenly split the panels' width into 2
+$('.panel').width($(window).width() / 2)
+
+//this is what will be displayed on the output when the page first renders
+updateOutput()
+
+//iframe contents wont update immediately without this code
+$('textarea').on('change keyup paste', function(){
+    updateOutput()
 })
